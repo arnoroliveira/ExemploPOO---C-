e347fde4f4185860c3adf196f5554941c49322a8
arnoroliveira <p.oliviras@gmail.com>
2024-10-19 20:44:35 -0300

    Add Módulo POO C#

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using ExemploPOO___C_;
+using ExemploPOO___C_.Models;
+using ExemploPOO___C_.interfaces;
+
+ICalculadora calculadora = new Calculadora();
+Console.WriteLine(calculadora.Multiplicar(3,9));
+
+
+
+
+
+
+
+
+
+
+
+// Computador c = new Computador();
+// Console.WriteLine(c.ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Corrente c = new Corrente();
+// c.Creditar(500);
+// c.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Aluno a1 = new Aluno();
+// a1.Nome = "Pedro";
+// a1.Idade = 22;
+// a1.Nota = 9.8;
+
+// a1.Apresentar();
+
+// Professor p1 = new Professor();
+// p1.Nome = "Arnor";
+// p1.Idade = 30;
+// p1.Salario = 1000;
+
+// p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(473, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Pedro";
+// p1.Idade = 22;
+
+// p1.Apresentar();
